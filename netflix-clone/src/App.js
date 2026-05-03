fb468354ab9bcee52f79f46d120fb8dda0ec9d7c
wead0909-ship-it <wead0909@gmail.com>
2026-05-03 16:07:08 -0700

    update-deploy

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,19 +1,28 @@
-// import './App.css';
-// import Home from './Pages/Home/Home';
+
+
+
+// import "./App.css";
+// import Home from "./Pages/Home/Home";
+// import Header from "./components/Header/Header";
+// import { Routes, Route } from "react-router-dom";
 
 // function App() {
 //   return (
 //     <div className="App">
-      
-//       <Home/>
+//       <Header />
 
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/movies" element={<Home />} />
+//         <Route path="/tv" element={<Home />} />
+//         <Route path="/latest" element={<Home />} />
+//         <Route path="/mylist" element={<Home />} />
+//       </Routes>
 //     </div>
 //   );
 // }
 
 // export default App;
-
-
 import "./App.css";
 import Home from "./Pages/Home/Home";
 import Header from "./components/Header/Header";
